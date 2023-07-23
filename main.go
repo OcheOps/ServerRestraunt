f709595532204843061265abe7adf39a40dfc1f7
ochecodes7 <ochecodes@gmail.com>
2023-07-24 00:40:15 +0100

    Endpoints

--- main.go
+++ main.go
@@ -18,3 +18,21 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
+	router.Use(cors.Default())
+
+
+	//C
+	router.POST("/order/create", routes.AddOrder)
+	//R
+	router.GET("/waiter/:waiter", routes.GetOrdersByWaiter)
+	router.GET("/orders", routes.GetOrders)
+	router.GET("/order/:id/", routes.GetOrderById)
+	//U
+	router.PUT("/waiter/update/:id", routes.UpdateWaiter)
+	router.PUT("/order/update/:id", routes.UpdateOrder)
+	//D
+	router.DELETE("/order/delete/:id", routes.DeleteOrder)
+
+	//this runs the server and allows it to listen to requests.
+	router.Run(":" + port)
+}
